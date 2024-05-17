79474da10ccf51ab7b0d0b37e198bc0583c0e171
MinhTran19102002 <20110677@student.hcmute.edu.vn>
2024-05-17 17:26:31 +0700

    first commit

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
-# from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 # from unit.test import select_image
-# import os
-# import numpy as np
-# from PIL import Image
-# import base64
-# import io
+import os
+import numpy as np
+from PIL import Image
+import base64
+import io
 
-from flask import Flask, render_template
+# from flask import Flask, render_template
 app = Flask(__name__)
-# app.config['UPLOAD_FOLDER'] = 'uploads'
-# os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
-# os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
+app.config['UPLOAD_FOLDER'] = 'uploads'
+os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 
 @app.route('/')
 def home():
